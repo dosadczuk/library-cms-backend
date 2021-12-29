919f5c7c0e6485543074e2596bd987c8373c842b
dosadczuk <damian.osadczuk@gmail.com>
2021-12-29 13:53:59 +0100

    Update SeedRatings

--- src/database/seeders/1640781210437-SeedRatings.ts
+++ src/database/seeders/1640781210437-SeedRatings.ts
@@ -1,9 +1,9 @@
 import { RatingSeed } from '@/database/seeders/seeds/rating.seed';
 import { Rating } from '@/modules/books/entities';
-import { getConnection, MigrationInterface, QueryRunner } from 'typeorm';
+import { getConnection, MigrationInterface } from 'typeorm';
 
 export class SeedRatings1640781210437 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
+  public async up(): Promise<void> {
     const connection = getConnection('seeder');
 
     return connection.transaction(async (manager) => {
@@ -11,7 +11,7 @@ export class SeedRatings1640781210437 implements MigrationInterface {
     });
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
+  public async down(): Promise<void> {
     // nothing
   }
 }
